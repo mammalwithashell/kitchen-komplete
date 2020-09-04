5e1f48c965a7d9a393980705d924b700d7e4c927
james <james.peters97@outlook.com>
2020-09-04 01:56:33 -0500

    Documentation updates and better comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
+"""__init__.py is a constructor document that lets the python interpreter 
+    see the folder named app as if it were a package that we can import objects from
+
+    First import the Flask class from the flask library
+    __name__ refers to the name in reference to the main python process
+
+    Next import the pages of the web app stored in routes
+    """
+from app import routes
 from flask import Flask
 
 app = Flask(__name__)
-from app import routes
---
+++ flask_env.sh
@@ -0,0 +1,1 @@
+FLASK_APP = "undecided_app.py"
